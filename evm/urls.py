5a977c5f1a5e3f5721c96f63dc115b2a9405b9a8
Sooryachandiran <12b23sooryachandirang@gmail.com>
2026-01-18 23:27:46 +0530

    created evm/urls.py and updated evm-project/urls.py

---
+++ evm/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'evm'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('cast-vote/<str:booth_id>/', views.cast_vote, name='cast_vote'),
+    path('status/<str:booth_id>/', views.booth_status, name='status'),
+    path('signals/<str:booth_id>/', views.booth_signals, name='signals'),
+    path('results/<str:booth_id>/', views.booth_results, name='results'),
+]
